f93b748cecf34951cbc5ee797ce31e6f9e143fd4
Supratim Saha <saha.supratim12@gmail.com>
2018-12-08 17:06:28 +0530

    Deploying on Herokuuuu..1!!

--- server.js
+++ server.js
@@ -25,10 +25,8 @@ server.use(bodyParser.json());
 const db = knex({
 	client : 'pg',
 	connection: {
-		host : '127.0.0.1',
-		user : 'postgres',
-		password : 'root',
-		database : 'brain'
+		connectionString : process.env.DATABASE_URL,
+		ssl : true
 	}
 });
 
